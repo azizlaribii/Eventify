a0a6eb0e1673f577340bf79762002a411f09360f
ounis <ounis.jihene24@gmail.com>
2025-12-11 16:12:44 +0100

    Ajout du projet dans la branche jihene

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./pages/**/*.html",
+    "./public/**/*.html",
+    "./src/**/*.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
